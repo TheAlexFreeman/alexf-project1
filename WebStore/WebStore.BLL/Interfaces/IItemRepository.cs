60fa8513714940100a2c606fdf2be7e950c9fe5d
TheAlexFreeman <alexrfreeman@protonmail.com>
2019-08-02 13:47:19 -0500

    Add ItemRepository interface

---
+++ WebStore/WebStore.BLL/Interfaces/IItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.BLL.Interfaces
+{
+    public interface IItemRepository
+    {
+        Item GetItemById(int id);
+        Item GetItemByName(string name);
+        IEnumerable<Item> SearchItemsByName(string search);
+        IEnumerable<Item> GetItemsInCostRange(double min, double max);
+
+        void AddItem(Item item);
+        void DeleteItem(int id);
+        void EditItem(int id, Item item);
+
+        void Save();
+    }
+}
--- WebStore/WebStore.Data/Repositories/ItemRepository.cs
+++ WebStore/WebStore.Data/Repositories/ItemRepository.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using WebStore.BLL;
+using WebStore.BLL.Interfaces;
 
 namespace WebStore.Data.Repositories
 {
-    public class ItemRepository
+    public class ItemRepository : IItemRepository
     {
         private readonly WebStoreContext _dbContext;
         public ItemRepository(WebStoreContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public void AddItem(BLL.Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditItem(int id, BLL.Item item)
+        {
+            throw new NotImplementedException();
         }
 
         public Item GetItemById(int id)
@@ -26,6 +42,31 @@ namespace WebStore.Data.Repositories
         public IEnumerable<Item> GetItems(string search = "")
         {
             return _dbContext.Items.Where(i => i.Name.Contains(search));
+        }
+
+        public IEnumerable<BLL.Item> GetItemsInCostRange(double min, double max)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BLL.Item> SearchItemsByName(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        BLL.Item IItemRepository.GetItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        BLL.Item IItemRepository.GetItemByName(string name)
+        {
+            throw new NotImplementedException();
         }
     }
 }
