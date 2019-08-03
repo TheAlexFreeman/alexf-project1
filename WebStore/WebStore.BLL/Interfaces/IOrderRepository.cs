3ac42e59f33665f4aa746f764dd10799482e19e3
TheAlexFreeman <alexrfreeman@protonmail.com>
2019-08-03 18:47:16 -0500

    Implemented order repository methods

--- WebStore/WebStore.BLL/Interfaces/IOrderRepository.cs
+++ WebStore/WebStore.BLL/Interfaces/IOrderRepository.cs
@@ -6,10 +6,12 @@ namespace WebStore.BLL.Interfaces
 {
     public interface IOrderRepository
     {
+        IEnumerable<Order> GetOrders();
         Order GetOrderById(int id);
         IEnumerable<Order> GetOrderHistoryByLocation(int locationId);
         IEnumerable<Order> GetOrderHistoryByCustomer(int customerId);
         IEnumerable<Order> GetOrderHistoryInRange(DateTime min, DateTime max);
         IEnumerable<Order> GetOrdersInPriceRange(double min, double max);
+        IEnumerable<Order> GetOrdersWithOpenState(bool isOpen);
     }
 }
--- WebStore/WebStore.Data/Repositories/OrderRepository.cs
+++ WebStore/WebStore.Data/Repositories/OrderRepository.cs
@@ -1,36 +1,90 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using WebStore.BLL;
 using WebStore.BLL.Interfaces;
 
 namespace WebStore.Data.Repositories
 {
-    public class OrderRepository : IOrderRepository
+    public class OrderRepository : Repository, IOrderRepository
     {
+        public OrderRepository(Entities.StoreDBContext dbContext) : base(dbContext) { }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return _dbContext.Order
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .Select(Mapper.Map);
+        }
         public Order GetOrderById(int id)
         {
-            throw new NotImplementedException();
+            var order = _dbContext.Order
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .FirstOrDefault(o => o.Id == id)
+                ?? throw new KeyNotFoundException($"No order with ID {id} exists in database");
+            return Mapper.Map(order);
         }
 
         public IEnumerable<Order> GetOrderHistoryByCustomer(int customerId)
         {
-            throw new NotImplementedException();
+            return _dbContext.Order
+                .Where(o => o.BuyerId == customerId)
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .Select(Mapper.Map);
         }
 
         public IEnumerable<Order> GetOrderHistoryByLocation(int locationId)
         {
-            throw new NotImplementedException();
+            return _dbContext.Order
+                .Where(o => o.SellerId == locationId)
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .Select(Mapper.Map);
         }
 
         public IEnumerable<Order> GetOrderHistoryInRange(DateTime min, DateTime max)
         {
-            throw new NotImplementedException();
+            return _dbContext.Order
+                .Where(o => o.LastModified >= min && o.LastModified <= max)
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .Select(Mapper.Map);
         }
-
         public IEnumerable<Order> GetOrdersInPriceRange(double min, double max)
         {
-            throw new NotImplementedException();
+            return _dbContext.Order
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .Select(Mapper.Map)
+                .Where(o => o.Total >= min && o.Total <= max);
+        }
+
+        public IEnumerable<Order> GetOrdersWithOpenState(bool isOpen)
+        {
+            return _dbContext.Order
+                .Where(o => o.IsOpen == isOpen)
+                .Include(o => o.Buyer)
+                .Include(o => o.Seller)
+                .Include(o => o.ProductOrder)
+                .ThenInclude(po => po.Product)
+                .Select(Mapper.Map);
         }
     }
 }
