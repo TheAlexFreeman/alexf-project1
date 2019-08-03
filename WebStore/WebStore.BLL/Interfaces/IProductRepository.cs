d7d17d96f9c389901ba89636c776c49f245cb547
TheAlexFreeman <alexrfreeman@protonmail.com>
2019-08-02 22:13:39 -0500

    Set up directory structure for views

---
+++ WebStore/WebStore.BLL/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.BLL.Interfaces
+{
+    interface IProductRepository
+    {
+        Product GetProductById(int id);
+        Product GetProductByName(string name);
+        IEnumerable<Product> GetProducts(string search = null);
+        IEnumerable<Product> GetProductsInPriceRange(double min, double max);
+    }
+}
