19488147b6009991e91ce3ea394f8c84c07dbe41
TheAlexFreeman <alexrfreeman@protonmail.com>
2019-08-03 17:44:32 -0500

    Implement location repository methods

--- WebStore/WebStore.Data/Repositories/LocationRepository.cs
+++ WebStore/WebStore.Data/Repositories/LocationRepository.cs
@@ -17,14 +17,22 @@ namespace WebStore.Data.Repositories
         }
         public Location GetLocationById(int id)
         {
-            var location = _dbContext.Location.Include(l => l.InventoryItem)
+            var location = _dbContext.Location
+                .Include(l => l.ProductLocation)
+                .ThenInclude(pl => pl.Product)
+                .Include(l => l.InventoryItem)
                 .ThenInclude(ii => ii.Item)
                 .FirstOrDefault(l => l.Id == id) ?? throw new KeyNotFoundException($"No location with ID {id} exists in database");
             return Mapper.Map(location);
         }
         public Location GetLocationByName(string name)
         {
-            var location = _dbContext.Location.FirstOrDefault(l => l.Name == name) ?? throw new KeyNotFoundException($"No location with name '{name}' exists in database");
+            var location = _dbContext.Location
+               .Include(l => l.ProductLocation)
+               .ThenInclude(pl => pl.Product)
+               .Include(l => l.InventoryItem)
+               .ThenInclude(ii => ii.Item)
+               .FirstOrDefault(l => l.Name == name) ?? throw new KeyNotFoundException($"No location with name '{name}' exists in database");
             return Mapper.Map(location);
         }
     }
